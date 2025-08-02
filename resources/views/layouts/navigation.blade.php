621eb35a12f433765b8104b2fa46441b77bf552f
fazilmeridian <fazilkalodi76@gmail.com>
2025-08-02 19:48:59 +0530

    feat: added checkout navigation link to the main navigation menu

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -12,8 +12,8 @@
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
+                    <x-nav-link :href="route('checkout')" :active="request()->routeIs('checkout')">
+                        {{ __('Checkout') }}
                     </x-nav-link>
                 </div>
             </div>
